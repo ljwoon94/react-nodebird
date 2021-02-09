1589a37d9fda54e762c0eb676ef046816fbc30e5
LeeJeongWoon <ljwoon94@gmail.com>
2021-02-09 14:23:15 +0900

    next 사용해서 hello world 작성

---
+++ prepare/front/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//next에선 import React를 반드시 할 필요가 없다.
+//next는 pages 폴더를 인식해서 개별적으로 페이지로 만든다.
+const Home = () =>{
+    return(
+        <div>Hello, Next!</div>
+    );
+}
+
+export default Home;
+
+
+// npm run dev 라고 입력하면 package.json 에 scripts에 정의해둔
+// 스크립트가 실행된다. dev 말고 정의해둔 값을 입력하면 스크립트 실행된다.  
