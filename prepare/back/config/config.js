a4e3f635e6530cbdc70d44b0ff370031c33a4e32
LeeJeongWoon <ljwoon94@gmail.com>
2021-03-26 17:43:58 +0900

    데이터베이스 이름 수정

--- prepare/back/config/config.js
+++ prepare/back/config/config.js
@@ -16,14 +16,14 @@ module.exports = {
   "test": {
     "username": "root",
     "password": process.env.DB_PASSWORD,
-    "database": "nodebird",
+    "database": "react-nodebird",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "password": process.env.DB_PASSWORD,
-    "database": "nodebird",
+    "database": "react-nodebird",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
