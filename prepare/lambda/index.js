5b74a4c652e757f69b86b7c47123dc62605fbafe
LeeJeongWoon <ljwoon94@gmail.com>
2021-03-31 21:19:16 +0900

    no message

--- prepare/lambda/index.js
+++ prepare/lambda/index.js
@@ -1,29 +1,29 @@
 const AWS = require('aws-sdk');
 const sharp = require('sharp');
 
-const s3 = new AWS.s3();
+const s3 = new AWS.S3();
 
-exports.handler =(event, context, callback) =>{
-    const Bucket = event.Records[0].s3.bucket.name; //jeongwoon-aws
-    const Key = decodeURIComponent(event.Records[0].s3.object.key); //original/12312312_abc.png
+exports.handler = async (event, context, callback) => {
+    const Bucket = event.Records[0].s3.bucket.name; // jeongwoon-sns
+    const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/12312312_abc.png
     console.log(Bucket, Key);
-    const filename = Key.split('/')[jKey.split('/').length-1];
-    const ext = Key.split('.')[Key.split('.').length-1].toLowerCase();
+    const filename = Key.split('/')[Key.split('/').length - 1];
+    const ext = Key.split('.')[Key.split('.').length - 1].toLowerCase();
     const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
     console.log('filename', filename, 'ext', ext);
 
     try {
-        const s3Object = await s3.getObject({Bucket, Key}).promise();
+        const s3Object = await s3.getObject({ Bucket, Key }).promise();
         console.log('original', s3Object.Body.length);
         const resizedImage = await sharp(s3Object.Body)
-            .resize(200, 200, {fit:'inside'})
+            .resize(400, 400, { fit: 'inside' })
             .toFormat(requiredFormat)
             .toBuffer();
         await s3.putObject({
             Bucket,
             Key: `thumb/${filename}`,
             Body: resizedImage,
-        }).promise();
+            }).promise();
         console.log('put', resizedImage.length);
         return callback(null, `thumb/${filename}`);
     } catch (error) {
